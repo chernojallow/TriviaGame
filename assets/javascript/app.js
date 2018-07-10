26ce7566d1d9629ffe9859bddd81b503d176c1b8
Zika Chea <jallsadou@gmail.com>
2018-07-09 23:14:20 -0500

    Commit Messages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,28 +17,28 @@
     var questions = ["Who is the current president of USA?", 
                          "What is the capital of Texas?", 
                          "In which Country are they playing the 2018  FIFA World Cup?", 
-                         "What month was the independence day of USA?", 
+                         "What is the capital of Gambia?", 
                          "Where do you shop for grocery?", 
                          "Which team do you support in the NBA?" ];
 
     var  options = [["Donald Trump", "George Bush", "Barrack Obama", "Bill Gates"], 
                        ["Dallas","Austin","Houston","San Antonio"], 
                        ["Japan", "Brazil", "Russia", "France"], 
-                       [ "June","August","July","May"], 
+                       [ "Bakau","Banjul","Dippa","Sukuta"], 
                        ["Doller General", "Doller Tree", "H.E.B", "Walmart"], 
                        ["Houston(Rockets)","Indiana(Pacers)","Golden States(Warriors)","Cleveland(Caveliers)"]];
                        
-   var  imageArray = ["<img class='center-block img-right' src='img/australia.png'>", 
-                     "<img class='center-block img-right' src='img/liberia.png'>", 
-                     "<img class='center-block img-right' src='img/taiwan.png'>", 
-                     "<img class='center-block img-right' src='img/japan.png'>", 
-                     "<img class='center-block img-right' src='img/china.png'>", 
-                     "<img class='center-block img-right' src='img/turkey.png'>"];
+   var  imageArray = ["<img class='center-block img-right' src='assets/images/image1.jpeg'>", 
+                     "<img class='center-block img-right' src='assets/images/image2.jpeg'>", 
+                     "<img class='center-block img-right' src='assets/images/image3.jpeg'>", 
+                     "<img class='center-block img-right' src='assets/images/image4.jpg'>", 
+                     "<img class='center-block img-right' src='assets/images/image7.jpeg'>", 
+                     "<img class='center-block img-right' src='assets/images/image6.jpeg'>"];
 
-   var   answers = ["A. Donald Trump", 
+   var   answers = [      "A. Donald Trump", 
                           "B. Austin", 
-                          "C. Russia", 
-                          "C. July", 
+                          "C.Russia", 
+                          "B. Banjul", 
                           "D. Walmart", 
                           "A. Houston(Rockets)" ];
                           
@@ -55,7 +55,7 @@
     
     //modified cherno
     function startGame() {
-        $(".mainArea").html ("<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>");
+        $(".bootstrap").html ("<p class='start'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start</a></p>");
     
     }
       startGame();
@@ -100,28 +100,37 @@
     // modified
     function timeOut() {
         notAnswered++;
-        $(".mainArea").html ("<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + count + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + answers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>");
-     var timer =  setTimeout(wait, 3000);  //  change to 4000 or other amount
+        $(".bootstrap").html 
+        ("<p class='time'>Time Remaining: <span class='timer'>" + count + "</span></p>" +
+          "<p class='text-center'>You ran out of time! The correct answer was: " + answers[questionCounter] + "</p>" +
+          "<img class='center-block img-wrong' src='img/x.png'>");
+     var timer =  setTimeout(wait, 2000);  //  change to 4000 or other amount
     }
     
     // modified
     function win() {
         rightAnswer++;
-        $(".mainArea").html  ("<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + count + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + answers[questionCounter] + "</p>" + imageArray[questionCounter]);
-     var timer= setTimeout(wait, 3000);  //  change to 4000 or other amount
+        $(".bootstrap").html  ("<p class='time'>Time Remaining: <span class='timer'>" + count + "</span></p>" + 
+        "<p class='text-center'>Correct! The answer is: " + answers[questionCounter] + "</p>" + imageArray[questionCounter]);
+       
+     var timer= setTimeout(wait, 2000);  //  change to 4000 or other amount
     }
     
     // modified
     function loss() {
         wrongAnswer++;
-        $(".mainArea").html("<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + count + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ answers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>");
-      var timer = setTimeout(wait, 3000); //  change to 4000 or other amount
+        $(".bootstrap").html("<p class=' time'>Time Remaining: <span class='timer'>" + count + "</span></p>" + 
+        "<p class='text-center'>Wrong! The correct answer is: "+ answers[questionCounter] + "</p>" + 
+        "<img class='center-block img-wrong' src='img/x.png'>");
+      var timer = setTimeout(wait, 2000); //  change to 4000 or other amount
     }
     
 
     // modified
     function domHTML() {
-        $(".mainArea").html ("<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questions[questionCounter] + "</p><p class='first-answer answer'>A. " + options[questionCounter][0] + "</p><p class='answer'>B. "+options[questionCounter][1]+"</p><p class='answer'>C. "+options[questionCounter][2]+"</p><p class='answer'>D. "+options[questionCounter][3]+"</p>");
+        $(".bootstrap").html ("<p class='time'>Time Remaining: <span class='timer'>20</span></p><p class='text-center'>" + 
+        questions[questionCounter] + "</p><p class='first-answer answer'>A. " + options[questionCounter][0] + 
+        "</p><p class='answer'>B. "+options[questionCounter][1]+"</p><p class='answer'>C."+options[questionCounter][2]+"</p><p class='answer'>D. "+options[questionCounter][3]+"</p>");
         
     }
     
@@ -157,13 +166,13 @@
     
     // modified
     function endGame() {
-        $(".mainArea").html 
-        ("<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + count + "</span></p>" + 
+        $(".bootstrap").html 
+        ("<p class=' time'>Time Remaining: <span class='timer'>" + count + "</span></p>" + 
         "<p class='text-center'>All done, here's how you did!" + "</p>" +  
         "<p class='summary-correct'>Correct Answers: " + rightAnswer + "</p>" +
          "<p>Wrong Answers: " + wrongAnswer+ "</p>" + 
          "<p>Unanswered: " + notAnswered + "</p>" + 
-         "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>");
+         "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Start Over!</a></p>");
         
     }
     
